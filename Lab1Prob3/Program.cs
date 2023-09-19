a5f24037e696c01056afe0d04dfa12f42687d39b
Ada Ababei <contact@adina_ababei@yahoo.com>
2023-09-19 18:55:42 +0300

    Modificare exercitiu 3

--- Lab1Prob3/Program.cs
+++ Lab1Prob3/Program.cs
@@ -8,6 +8,24 @@ namespace Lab1Prob3
     {
         static void Main(string[] args)
         {
+            /*Ex3
+Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la
+tastaura
+ (google: how to get the last digit of a number)
+            */
+            int numar = int.Parse(Console.ReadLine());
+            int ultimaCifra = 0;
+
+            if (numar/2 = 0)
+            {
+
+                ultimaCifra = numar % 10;
+                Console.WriteLine("Ultima cifra este =" + ultimaCifra);
+            }
+            else
+            {
+                Console.WriteLine("Numarul nu este intreg" + numar);
+            }
         }
     }
 }
